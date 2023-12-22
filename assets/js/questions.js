4efc9e10bc27540e03ab8fc805c3782e4efbb211
Michal Paleczny <paleczny.michal@gmail.com>
2023-12-22 20:00:14 +0100

    added questions to js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,16 +2,55 @@
 
 // Array of quiz questions
 const questions = [
-    {
-      title: 'What is the capital of France?',
-      choices: ['Berlin', 'Madrid', 'Paris', 'Rome'],
-      correctAnswer: 2,
-    },
-    {
-      title: 'Which programming language is often used for web development?',
-      choices: ['Java', 'Python', 'JavaScript', 'C++'],
-      correctAnswer: 2,
-    },
-    // Add more questions as needed
+  {
+    title: 'What does HTML stand for?',
+    choices: ['Hyper Text Markup Language', 'High-level Text Machine Language', 'Hyperlink and Text Markup Language', 'Home Tool Markup Language'],
+    correctAnswer: 0,
+  },
+  {
+    title: 'Which of the following is a semantic element in HTML?',
+    choices: ['<div>', '<span>', '<article>', '<section>'],
+    correctAnswer: 2,
+  },
+  {
+    title: 'What does CSS stand for?',
+    choices: ['Counter Strike: Source', 'Corrective Style Sheet', 'Computer Style Sheet', 'Cascading Style Sheet'],
+    correctAnswer: 3,
+  },
+  {
+    title: 'Which CSS property is used for changing the text color of an element?',
+    choices: ['color', 'text-color', 'font-color', 'foreground-color'],
+    correctAnswer: 0,
+  },
+  {
+    title: 'What is the purpose of the Bootstrap framework?',
+    choices: ['Server-side scripting', 'Front-end development', 'Database management', 'Networking'],
+    correctAnswer: 1,
+  },
+  {
+    title: 'In JavaScript, what is the purpose of the "addEventListener" method?',
+    choices: ['To add two numbers', 'To create an array', 'To handle events', 'To change the DOM structure'],
+    correctAnswer: 2,
+  },
+  {
+    title: 'Which operator is used for equality comparison in JavaScript?',
+    choices: ['=', '==', '===', '!='],
+    correctAnswer: 2,
+  },
+  {
+    title: 'What does the acronym DOM stand for?',
+    choices: ['Document Object Model', 'Data Object Model', 'Digital Output Module', 'Distributed Order Management'],
+    correctAnswer: 0,
+  },
+  {
+    title: 'Which of the following is NOT a valid JavaScript data type?',
+    choices: ['string', 'boolean', 'number', 'tuple'],
+    correctAnswer: 3,
+  },
+  {
+    title: 'What is the purpose of the "media queries" in CSS?',
+    choices: ['To create animations', 'To print the document', 'To apply styles based on device characteristics', 'To compress images'],
+    correctAnswer: 2,
+  },
   ];
   
