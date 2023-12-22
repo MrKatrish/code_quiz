bc341624e4604508c5a3a183403a355277ca4753
Michal Paleczny <paleczny.michal@gmail.com>
2023-12-22 20:42:42 +0100

    finished logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -73,7 +73,7 @@ function checkAnswer(choiceIndex) {
     // Increment score for correct answer
     score++;
   }
-  
+
   // Move to the next question
   currentQuestionIndex++;
 
@@ -97,3 +97,20 @@ function endQuiz() {
   // Display final score
   document.getElementById('final-score').textContent = score;
 }
+
+// Function to save the score
+function saveScore() {
+  const initialsInput = document.getElementById('initials');
+  const initials = initialsInput.value.trim();
+
+  if (initials !== '') {
+    // Save the score and initials (you may use local storage or send it to a server)
+    // Example: localStorage.setItem('highScore', JSON.stringify({ initials, score }));
+    alert(`Score saved for ${initials}!`);
+  } else {
+    alert('Please enter your initials.');
+  }
+}
+
+// Event listener for the submit button
+document.getElementById('submit').addEventListener('click', saveScore);
