06fd611b81323fa0dd9f0b27d22a9e87201194cf
Michal Paleczny <paleczny.michal@gmail.com>
2023-12-28 19:37:20 +0100

    Add the final 10 questions to question.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -52,5 +52,55 @@ const questions = [
     choices: ['To create animations', 'To print the document', 'To apply styles based on device characteristics', 'To compress images'],
     correctAnswer: 2,
   },
+  {
+    title: 'What new semantic elements were introduced in HTML5?',
+    choices: ['<article> and <section>', '<div> and <span>', '<header> and <footer>', '<nav> and <aside>'],
+    correctAnswer: 0,
+  },
+  {
+    title: 'In CSS3, what is the purpose of the "flexbox" layout?',
+    choices: ['Creating responsive images', 'Styling form elements', 'Designing grid layouts', 'Building flexible and efficient page layouts'],
+    correctAnswer: 3,
+  },
+  {
+    title: 'Which HTML5 API is used for offline web application functionality?',
+    choices: ['WebSocket API', 'Canvas API', 'Geolocation API', 'Service Workers API'],
+    correctAnswer: 3,
+  },
+  {
+    title: 'What is the significance of the "box-sizing" property in CSS3?',
+    choices: ['Adjusting text spacing', 'Controlling element visibility', 'Defining box shadows', 'Managing box dimensions'],
+    correctAnswer: 3,
+  },
+  {
+    title: 'In JavaScript, what is the purpose of the "localStorage" object?',
+    choices: ['Storing data on the server side', 'Managing session cookies', 'Handling form submissions', 'Storing data on the client side'],
+    correctAnswer: 3,
+  },
+  {
+    title: 'Which CSS3 feature is used for applying styles to specific viewport sizes?',
+    choices: ['Media queries', 'Transforms', 'Transitions', 'Grid layout'],
+    correctAnswer: 0,
+  },
+  {
+    title: 'What is the purpose of the HTML5 "data-*" attribute?',
+    choices: ['Defining metadata for search engines', 'Storing custom data private to the page or application', 'Creating custom font styles', 'Embedding external scripts'],
+    correctAnswer: 1,
+  },
+  {
+    title: 'In CSS3, what is the "transition" property used for?',
+    choices: ['Creating animated effects', 'Specifying font styles', 'Applying color gradients', 'Defining responsive layouts'],
+    correctAnswer: 0,
+  },
+  {
+    title: 'What is the role of the "async" attribute in the HTML5 "script" element?',
+    choices: ['Defining a script file', 'Loading the script asynchronously', 'Setting script visibility', 'Deferring script execution'],
+    correctAnswer: 1,
+  },
+  {
+    title: 'In JavaScript, what is the purpose of the "querySelector" method?',
+    choices: ['Selecting elements with a specific class', 'Iterating over array elements', 'Querying the DOM for elements with a specified selector', 'Creating new HTML elements'],
+    correctAnswer: 2,
+  },
   ];
   
